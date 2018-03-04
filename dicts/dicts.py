de4efa5e56a5130cc0c70088c933085f5b8cefab
Martin Unzner <martin.u@posteo.de>
2018-03-04 19:06:31 +0100

    Allow for multiple dictionaries

--- dicts/dicts.py
+++ dicts/dicts.py
@@ -1,8 +1,8 @@
 class Dicts:
-    def __init__(self, active_table):
-        self._active_table = active_table
+    def __init__(self, active_tables):
+        self._active_tables = active_tables
 
     def lookup(self, word):
-        return self._active_table.lookup(word) \
-                if self._active_table \
-                else word
+        return [active_table.lookup(word) for active_table in self._active_tables] \
+                if self._active_tables \
+                else [word]
--- dicts_tests.py
+++ dicts_tests.py
@@ -13,6 +13,17 @@ class ExampleTable1:
             return self.BREAKFAST_TRANS
 
 
+class ExampleTable2:
+    SPAM_TRANS = ['SPAMO', 'SPAMI' 'SPAMA']
+    BREAKFAST_TRANS = ['egge', 'baco', 'span']
+
+    def lookup(self, word):
+        if word == 'spam':
+            return self.SPAM_TRANS
+        elif word == 'breakfast':
+            return self.BREAKFAST_TRANS
+
+
 class TestDicts(unittest.TestCase):
     def test_lookup_exists(self):
         dicts = Dicts(None)
@@ -20,12 +31,21 @@ class TestDicts(unittest.TestCase):
 
     def test_lookup_returns_word_unchanged_without_tables(self):
         dicts = Dicts(None)
-        self.assertEqual('gnarble', dicts.lookup('gnarble'))
+        self.assertEqual(['gnarble'], dicts.lookup('gnarble'))
 
     def test_return_expected_translation(self):
-        dicts = Dicts(ExampleTable1())
-        self.assertEqual(ExampleTable1.SPAM_TRANS, dicts.lookup('spam'))
-        self.assertEqual(ExampleTable1.BREAKFAST_TRANS, dicts.lookup('breakfast'))
+        dicts = Dicts([ExampleTable1()])
+        self.assertEqual([ExampleTable1.SPAM_TRANS], dicts.lookup('spam'))
+        self.assertEqual([ExampleTable1.BREAKFAST_TRANS], dicts.lookup('breakfast'))
+
+    def test_return_translations_from_multiple_tables(self):
+        dicts = Dicts([ExampleTable1(), ExampleTable2()])
+        self.assertEqual(
+                [ExampleTable1.SPAM_TRANS, ExampleTable2.SPAM_TRANS],
+                dicts.lookup('spam'))
+        self.assertEqual(
+                [ExampleTable1.BREAKFAST_TRANS, ExampleTable2.BREAKFAST_TRANS],
+                dicts.lookup('breakfast'))
 
 if __name__ == '__main__':
     unittest.main()
