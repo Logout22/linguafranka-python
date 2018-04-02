bbd5a271f020cb64dcad0bc60c9e8eb6173f4a8f
Martin Unzner <martin.u@posteo.de>
2018-04-02 17:36:22 +0200

    Start support for nested tags

--- dicts/pons_parser.py
+++ dicts/pons_parser.py
@@ -44,11 +44,41 @@ Test
         pons_parser.feed(tag_string)
         self.assertEqual(tags, pons_parser.lookup_table)
 
+    def test_find_actual_translation(self):
+        tag_string = """
+<dl id="Tdeen1328772" class="dl-horizontal kne first" data-translation="13">
+  <dt>
+    <div class="dt-inner">
+      <ul class="inline translation-options">
+<li class="dropdown"><a href="#" class="dropdown-toggle tts trackable trk-audio" data-toggle="dropdown"><i class="icon-volume-down"></i></a><ul class="dropdown-menu"><li><a href="#" class="audio tts trackable trk-audio" data-pons-lang="en_gb"><span class="flag flag_en"></span> Britisches Englisch</a></li><li><a href="#" class="audio tts trackable trk-audio" data-pons-lang="en_us"><span class="flag flag_us"></span> Amerikanisches Englisch</a></li></ul></li>      </ul>
+      <div class="source">
+<strong class="headword"><a href="/übersetzung?l=deen&amp;q=test&amp;in=en">test</a></strong>      </div>
+    </div>
+  </dt>
+  <dd>
+    <div class="dd-inner">
+      <div class="target">
+<a href="/%C3%BCbersetzung/deutsch-englisch/Untersuchung">Untersuchung</a> <span class="genus"><acronym title="Femininum">f</acronym></span>      </div>
+      <ul class="inline translation-options">
+<li><a href="#" class="audio tts trackable trk-audio" data-pons-lang="de"><i class="icon-volume-down"></i></a></li><li class="dropdown"><a href="#" data-toggle="dropdown"><i class="icon-plus-sign"></i></a><ul class="dropdown-menu"><li><a href="#" class="favorites"><i class="icon-star"></i>&nbsp;Zu meinen Favoriten hinzufügen</a></li><li><a href="#" class="trainer"><i class="icon-inbox"></i>&nbsp;Für den Vokabeltrainer vormerken</a></li><li><a href="/trainer-import" class="trainer-import"><i class="icon-inbox"></i>&nbsp;Vorgemerkte Vokabeln ansehen</a></li></ul></li>      </ul>
+    </div>
+  </dd>
+  
+</dl>
+        """
+        tags = {"test": ["Untersuchung"]}
+        pons_parser = PonsParser()
+        pons_parser.feed(tag_string)
+        self.assertEqual(tags, pons_parser.lookup_table)
+
 
 class PonsParserStates(Enum):
     SEARCHING = 0
-    IN_DT = 1
-    IN_DD = 2
+    IN_DT = 10
+    IN_SOURCE = 15
+    IN_DD = 20
+    IN_TARGET = 25
+
 
 class PonsParser(HTMLParser):
     def __init__(self):
@@ -62,9 +92,17 @@ class PonsParser(HTMLParser):
             self.state = PonsParserStates.IN_DT
         elif tag == 'dd':
             self.state = PonsParserStates.IN_DD
+        elif tag == 'div' and \
+                self.state == PonsParserStates.IN_DT and \
+                ('class', 'source') in attrs:
+            self.state = PonsParserStates.IN_SOURCE
 
     def handle_endtag(self, tag):
-        self.state = PonsParserStates.SEARCHING
+        if (tag == 'dt' and self.state == PonsParserStates.IN_DT) or \
+                (tag == 'dd' and self.state == PonsParserStates.IN_DD) or \
+                (tag == 'div' and self.state == PonsParserStates.IN_SOURCE) or \
+                (tag == 'div' and self.state == PonsParserStates.IN_TARGET):
+            self.state = PonsParserStates.SEARCHING
 
     def handle_data(self, data):
         if self.state == PonsParserStates.IN_DT:
